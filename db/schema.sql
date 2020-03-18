b7e08d6bac8ac3733dc680eb8a9886d5aa9a5549
acarrillo2019 <alric21@aol.com>
2020-03-18 08:10:36 -0700

    Adjusted schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,3 @@
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS burgers2_db;
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
--- Create the burger table.
-CREATE TABLE burgers
-(
-id INT NOT NULL AUTO_INCREMENT,
-burger_name VARCHAR (255) NOT NULL,
-devoured BOOLEAN DEFAULT false,
-PRIMARY KEY (id)
-);
-
-SELECT * FROM burgers
-
-CREATE TABLE burger (
-id INT NOT NULL AUTO_INCREMENT,
-burger_name VARCHAR (255) NOT NULL,
-devoured BOOLEAN DEFAULT false,
-PRIMARY KEY(id)
-);
-SELECT * FROM burger
+CREATE DATABASE burgers2_db;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 -- Insert a set of records.
-INSERT INTO burgers (burger_name, devoured) VALUES ('Crabby Patty', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Yum Yum Jr.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jumbo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bronto Burger', false);
